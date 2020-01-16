949f97893a8246fcbc5da6f8e168e27aca2a9730
PrateekShrivastawa <59281742+PrateekShrivastawa@users.noreply.github.com>
2020-01-16 21:35:45 +0530

    Update intsorting.java

--- intsorting.java
+++ intsorting.java
@@ -1,30 +1,32 @@
-package com.company;
-
 import java.util.Scanner;
 
 public class intsorting {
     public static void main(String[] args) {
         int temp;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array");
+        System.out.println("Enter the size of array");// it input the size of the array 
         int a = sc.nextInt();
         int[] arr = new int[a];
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) // this loop is use for enter the element till the size of the array
+        {
             System.out.println("Enter the " + (i + 1) + " element");
             arr[i] = sc.nextInt();
         }
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    temp = arr[j];
+            for (int j = 0; j < arr.length - 1 - i; j++) // t
+            {
+                if (arr[j] > arr[j + 1]) // 
+                {
+                    temp = arr[j];// here we take a temp variable for swapping the values
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
         }
         System.out.println("The sorted array is given below");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for (int i = 0; i < arr.length; i++) // This loop prints the sorted loop 
+        {
+            System.out.print(arr[i] + " ");// here print() function prints the sorted loop on the single line
         }
 
     }
